20e95386995740f987aadcf77ff8c86d8c03f562
dylan salay <dylansalay@gmail.com>
2020-09-12 09:49:28 -0700

    Add placeholder images for inventory items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |item|
+  InventoryItem.create!(
+    title: "Inventory Item #{item}",
+    subtitle: "Inventory Item subtitle #{item}",
+    body: "lorem ipsum",
+    unit_cost: "#{item}",
+    main_image: "https://placehold.it/600x400",
+    thumb_image: "https://placehold.it/350x200"
+  )
+end

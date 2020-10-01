16082504bf919cacd4fa6a6f30673071c9c09379
dylan salay <dylansalay@gmail.com>
2020-09-30 18:15:34 -0700

    reconfigure user name to first and last name

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,13 +7,15 @@ FactoryBot.define do
 
   factory :user do
     email { generate :email }
-    name { 'Jon Snow' }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     password { 'asdfasdf' }
   end
 
   factory :admin_user, class: 'AdminUser' do
     email { generate :email }
-    name { 'Admin User' }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     password { 'asdfasdf' }
   end
 end
--- spec/models/inventory_item_spec.rb
+++ spec/models/inventory_item_spec.rb
@@ -1,27 +1,29 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe InventoryItem, type: :model do
-  describe "Creation" do
-  	before do
-			@inventory_item = FactoryBot.create(:inventory_item)
-  	end
+  describe 'Creation' do
+    before do
+      @inventory_item = FactoryBot.create(:inventory_item)
+    end
 
-		it 'can be created' do	
-  		expect(@inventory_item).to be_valid
-  	end
+    it 'can be created' do
+      expect(@inventory_item).to be_valid
+    end
 
-  	it 'cannot be created without a title or body' do
-  		@inventory_item.title = nil
-  		@inventory_item.body = nil
-  		expect(@inventory_item).to_not be_valid
-		end
-		
-		it 'sets default values for main_image and thumb_image' do
-			@inventory_item.main_image = nil 
-      @inventory_item.thumb_image = nil 
+    it 'cannot be created without a title or body' do
+      @inventory_item.title = nil
+      @inventory_item.body = nil
+      expect(@inventory_item).to_not be_valid
+    end
+
+    it 'sets default values for main_image and thumb_image' do
+      @inventory_item.main_image = nil
+      @inventory_item.thumb_image = nil
       @inventory_item.reload
-			expect(@inventory_item.main_image).to eq(Placeholder.image_generator(height: '600', width: '400'))
-			expect(@inventory_item.thumb_image).to eq(Placeholder.image_generator(height: '350', width: '200'))
-		end
+      expect(@inventory_item.main_image).to eq(Placeholder.image_generator(height: '600', width: '400'))
+      expect(@inventory_item.thumb_image).to eq(Placeholder.image_generator(height: '350', width: '200'))
+    end
   end
 end

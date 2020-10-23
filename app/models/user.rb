3212d1241b58c4a6fc95d8dcd4bbff0872676a89
dylan salay <dylansalay@gmail.com>
2020-10-23 16:42:03 -0700

    Initial styles on navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def name
+    "#{self.first_name + " " + self.last_name}"
+  end
 end

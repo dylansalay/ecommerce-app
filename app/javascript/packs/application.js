021b4c69c6a5d9b2994793856f692b9b6fa3a950
dylan salay <dylansalay@gmail.com>
2021-02-05 14:33:14 -0800

    Add Stimulus JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,9 +15,7 @@ import "slick-carousel/slick/slick-theme.scss"
 import "../styles/application.scss"
 import 'bootstrap'
 import "@fortawesome/fontawesome-free/css/all";
-
-
-
+import "controllers"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)

c9ce102fcc800aa298ab92a02e1621af18c53a7e
dylan salay <dylansalay@gmail.com>
2021-02-19 13:52:26 -0800

    Set checkbox to false each time page is loaded

--- app/javascript/controllers/shopping-cart_controller.js
+++ app/javascript/controllers/shopping-cart_controller.js
@@ -2,11 +2,16 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   static targets = [ "billing_same_as_shipping" ]
-  toggle(event) {
 
+  connect() {
+    this.billing_same_as_shippingTarget.checked = false
+  }
+
+  toggle(event) {
     // If checked, hide billing address form
     var billing = document.getElementsByClassName("billing-address")
     if(event.target.checked) {
+      console.log("true")
       billing[0].classList.add('hide')
     } else {
       billing[0].classList.remove('hide')

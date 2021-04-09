ed15fc518f6c05159dd689a05eecb461375fb28f
dylan salay <dylansalay@gmail.com>
2021-04-09 14:08:04 -0700

    clean up inv styles

--- app/helpers/inventory_items_helper.rb
+++ app/helpers/inventory_items_helper.rb
@@ -19,6 +19,19 @@ module InventoryItemsHelper
     end
   end
 
+  def item_style(item) 
+    options = ['Select Style', 'Add Style']
+    if item.styles.any?
+      styles = (options + item.styles.map{ |s| ["#{s.color + ', ' + s.stone}", s.id] })
+    end
+    
+    select_tag(
+      :style, 
+      options_for_select(styles, "Select Style"), 
+      data: { action: "change->inventory-item#select", "inventory-item-target"=>"select_dropdown" }
+    )
+  end
+
   def inventory_badge_helper
     load_shopping_cart.cart_quantity unless load_shopping_cart.cart_quantity.zero?
   end
--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -7,7 +7,7 @@ class InventoryItem < ApplicationRecord
   belongs_to :category
   belongs_to :shopping_cart, optional: true
 
-  has_many :styles, dependent: :destroy, autosave: true, class_name: 'Style'
+  has_many :styles, dependent: :destroy, class_name: 'Style'
 
   accepts_nested_attributes_for :styles, allow_destroy: true
 
@@ -19,8 +19,12 @@ class InventoryItem < ApplicationRecord
 
   def styles_options_for_select
     options = []
-    styles.each do |style|
-      options << ["#{style.length} | #{style.color}" + (style.stone.present? ? " | #{style.stone}" : '')]
+    if styles.any?
+      styles.each do |style|
+        options << ["#{style.length} | #{style.color}" + (style.stone.present? ? " | #{style.stone}" : '')]
+      end
+    else
+      options << 'One Style'
     end
     options
   end

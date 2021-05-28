612250315061bf666429ebec1473513e449fd835
dylan salay <dylansalay@gmail.com>
2021-05-28 15:10:16 -0700

    Add stimulus js code for star ratings

--- app/javascript/controllers/reset-form_controller.js
+++ app/javascript/controllers/reset-form_controller.js
@@ -1,7 +1,12 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
- reset() {
-   this.element.reset()
- }
+  reset() {
+    this.element.reset()
+    const stars = document.getElementsByClassName("rating-stars")
+    Array.from(stars).forEach( star => {
+      star.classList.remove('fas')
+      star.classList.add('far')
+    })
+  }
 }
